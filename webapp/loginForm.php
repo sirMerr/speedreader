cf43b48b4665636b70aa0e14f8ffa80e4806e437
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-11-12 15:50:17 -0500

    :wrench: Test login

--- webapp/loginForm.php
+++ webapp/loginForm.php
@@ -23,17 +23,37 @@ login($username, $password);
 function login($username, $password) {
     $dao = new DAO();
 
+    echo "
+    <div class='container'>
+    <h1>In login: $username , $password</h1>
+    </div>
+    ";
     // Use DAO to get login attempts
     if ($dao->findLoginAttempts($username) >= 3) {
-    // redirect to error page
+        // redirect to error page
+        echo "
+        <div class='container'>
+        <h1>More than 3 login attempts</h1>
+        </div>
+        ";
 
     } else {
         $hash = $dao->findHash($username);
+        echo "
+            <div class='container'>
+            <h1>Hash: $hash</h1>
+            </div>
+            ";
         // Authenticate user, update time
         if (!password_verify($password, $hash)) {
             // Increment attempts
             $dao->updateLoginAttemptsIncrement($username);
             // Redirect to error page, or back to login with error message
+            echo "
+            <div class='container'>
+            <h1>Invalid login, incrementing login attempts.</h1>
+            </div>
+            ";
         } else {
             $dao->updateResetLoginAttempts($username);
             // Reset attempts
