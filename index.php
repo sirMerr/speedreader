464b13f8468830ab3f241ad03db19fae1bfc9ff7
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-11-12 16:40:37 -0500

    :wrench: Test more session

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
-// Start session
 session_start();
 
 echo "
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' integrity='sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb' crossorigin='anonymous'>
 ";
 
-print_r($_SESSION);
 
 if (isset($_SESSION['username'])) {
     // Set profile
