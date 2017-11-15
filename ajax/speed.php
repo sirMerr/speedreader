16215c34c2f1e2824ee38f6ff241badf7bb3c857
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-11-15 15:13:33 -0500

    :fire: Remove unused ajax files

--- ajax/speed.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-require_once(__DIR__ . '/../persistence/DAO.php');
-
-$dao = new DAO();
-
-$speed = $dao->findSpeed($_SESSION['username']);
-$dao->closeConnection();
-
-echo $speed;
--- ajax/updateSpeed.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-require_once(__DIR__ . '/../persistence/DAO.php');
-
-if (isset($_POST['speed'])) {
-    $dao = new DAO();
-    $dao->updateSpeed($_SESSION['username'], $_POST['speed']);
-    $dao->closeConnection();
-}
